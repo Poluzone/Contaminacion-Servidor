99532afaf3b5dba5e782b1ccf8035651a4d7aa26
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-11-16 23:57:37 +0100

    get todos los sensores implementado

--- Servidor/logica/Logica.js
+++ Servidor/logica/Logica.js
@@ -29,20 +29,20 @@ module.exports = class Logica {
       $IdMedida: null,
       $IdTipoMedida: medida.IdTipoMedida,
       $IdUsuario: medida.IdUsuario,
-      $Valor:medida.Valor,
-      $Tiempo:medida.Tiempo,
-      $Latitud:medida.Latitud,
-      $Longitud:medida.Longitud
+      $Valor: medida.Valor,
+      $Tiempo: medida.Tiempo,
+      $Latitud: medida.Latitud,
+      $Longitud: medida.Longitud
     }
     return new Promise((resolver, rechazar) => {
-      this.laConexion.run(textoSQL, valoresParaSQL, function(err) {
+      this.laConexion.run(textoSQL, valoresParaSQL, function (err) {
         (err ? rechazar(err) : resolver())
       })
     })
   }
 
   async getLaUltimaMedidaPorUsuario(userId) {
-    var textoSQL = "SELECT * FROM Medidas WHERE IdUsuario="+userId+" ORDER BY IdMedida DESC LIMIT 0, 1";
+    var textoSQL = "SELECT * FROM Medidas WHERE IdUsuario=" + userId + " ORDER BY IdMedida DESC LIMIT 0, 1";
     return new Promise((resolver, rechazar) => {
       this.laConexion.all(textoSQL,
         (err, res) => {
@@ -62,7 +62,7 @@ module.exports = class Logica {
   }*/
 
   async GetIdDelUsuario(email) {
-    var textoSQL = "SELECT * FROM Usuarios WHERE Email='"+email+"';";
+    var textoSQL = "SELECT * FROM Usuarios WHERE Email='" + email + "';";
     return new Promise((resolver, rechazar) => {
       this.laConexion.all(textoSQL,
         (err, res) => {
@@ -71,29 +71,98 @@ module.exports = class Logica {
     })
   }
 
+  // .................................................................
+  // email -> GetHashPorEmail() ->
+  // .................................................................
   async GetHashPorEmail(email) {
-//  var textoSQL = "select Password from Usuarios where Email = $email ";
+    //  var textoSQL = "select Password from Usuarios where Email = $email ";
     var valoresParaSQL = {
       $email: email
     };
     return new Promise((resolver, rechazar) => {
-      this.laConexion.all(textoSQL,valoresParaSQL,
+      this.laConexion.all(textoSQL, valoresParaSQL,
         (err, res) => {
           (err ? rechazar(err) : resolver(res))
         })
     })
   }
 
-async GetUsuarioPorEmail(email) {
+  // .................................................................
+  // email -> GetUsuarioPorEmail() ->
+  // .................................................................
+  async GetUsuarioPorEmail(email) {
     var textoSQL = "select * from Usuarios where Email = $email ";
-    var valoresParaSQL = {  $email: email };
+    var valoresParaSQL = { $email: email };
     return new Promise((resolver, rechazar) => {
-      this.laConexion.all(textoSQL,valoresParaSQL,
-          (err, res) => {
+      this.laConexion.all(textoSQL, valoresParaSQL,
+        (err, res) => {
           (err ? rechazar(err) : resolver(res))
         })
     })
   }
+
+  // .................................................................
+  // -> getSensoresYSusUsuarios() ->
+  // llama a getTodosLosSensores
+  // y getUsuarioPorIdSensor
+  // .................................................................
+  async getSensoresYSusUsuarios() {
+    var sensores = await this.getTodosLosSensores();
+    for (var i = 0; i < sensores.length; i++) {
+      var sensor = sensores[i]
+      var idSensor = sensor.IdSensor;
+      var usuario = await this.getUsuarioPorIdSensor(idSensor);
+      sensores[i].Usuario = usuario[0];
+    }
+    console.log(sensores)
+    return new Promise((resolver, rechazar) => {
+      resolver(sensores)
+    })
+  }
+
+  // .................................................................
+  // -> getTodosLosSensores() ->
+  // Coge la info de todos los sensores
+  // .................................................................
+  async getTodosLosSensores() {
+    var textoSQL = "select * from Sensor";
+    console.log("logica: getTodosLosSensores")
+    return new Promise((resolver, rechazar) => {
+      this.laConexion.all(textoSQL, {},
+        (err, res) => {
+          (err ? rechazar(err) : resolver(res))
+        })
+    })
+  }
+
+  // .................................................................
+  // -> getUsuarioPorIdSensor(idSensor) ->
+  // Coge la info de un usuario a partir del id de su sensor
+  // .................................................................
+  async getUsuarioPorIdSensor(idSensor) {
+    var textoSQL = "select IdUsuario from UsuarioSensor where IdSensor = $idSensor ";
+    var valoresParaSQL = { $idSensor: idSensor };
+    return new Promise((resolver, rechazar) => {
+      this.laConexion.all(textoSQL, valoresParaSQL,
+        (err, res) => {
+          if (err) console.log(err)
+          else {
+            var id = res[0].IdUsuario
+            textoSQL = "select * from Usuarios where IdUsuario = $id";
+            valoresParaSQL = { $id: id };
+            this.laConexion.all(textoSQL, valoresParaSQL,
+              (err, res) => {
+                (err ? rechazar(err) : resolver(res))
+              })
+          }
+        }
+      )
+    })
+  }
+
+  // .................................................................
+  // datos -> insertarUsuario() ->
+  // .................................................................
 
   insertarUsuario(datos) {
     var textoSQL = "insert into Usuarios values( $IdUsuario, $Email, $Password, $Nombre, $Telefono, $TipoUsuario)";
@@ -106,10 +175,10 @@ async GetUsuarioPorEmail(email) {
       $TipoUsuario: datos.TipoUsuario
     };
     return new Promise((resolver, rechazar) => {
-      this.laConexion.run(textoSQL, valoresParaSQL, function(err, res) {
+      this.laConexion.run(textoSQL, valoresParaSQL, function (err, res) {
         (err ? rechazar(err) : resolver(res))
       })
-     })
+    })
   }
 
   async borrarFilasDe(tabla) {
@@ -121,7 +190,7 @@ async GetUsuarioPorEmail(email) {
     })
   } // ()
   // .................................................................
-  // borrarFilasDeTodasLasTablas() -->
+  // borrarFilasDeTodasLasTablas() ->
   // .................................................................
   async borrarFilasDeTodasLasTablas() {
     await this.borrarFilasDe("Usuarios")
