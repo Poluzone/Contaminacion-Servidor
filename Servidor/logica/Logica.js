97c561aea0246ee10691d6880554f9f98e206a04
Diego <aguirre.dial@gmail.com>
2019-11-18 11:09:17 +0100

    Empezando implementación GetMedidaPorFecha

--- Servidor/logica/Logica.js
+++ Servidor/logica/Logica.js
@@ -6,52 +6,67 @@ const sqlite3 = require("sqlite3")
 // .....................................................................
 // .....................................................................
 module.exports = class Logica {
-  // .................................................................
-  // nombreBD: Texto
-  // -->
-  // constructor () -->
-  // .................................................................
-  constructor(nombreBD, cb) {
-    this.laConexion = new sqlite3.Database(
-      nombreBD,
-      (err) => {
-        if (!err) {
-          this.laConexion.run("PRAGMA foreign_keys = ON")
+    // .................................................................
+    // nombreBD: Texto
+    // -->
+    // constructor () -->
+    // .................................................................
+    constructor(nombreBD, cb) {
+        this.laConexion = new sqlite3.Database(
+            nombreBD,
+            (err) => {
+                if (!err) {
+                    this.laConexion.run("PRAGMA foreign_keys = ON")
+                }
+                cb(err)
+            })
+    } // ()
+
+    async insertarMedida(medida) {
+        var textoSQL =
+            'insert into Medidas values( $IdMedida, $IdTipoMedida , $IdUsuario, $Valor , $Tiempo , $Latitud , $Longitud);'
+        var valoresParaSQL = {
+            $IdMedida: null,
+            $IdTipoMedida: medida.IdTipoMedida,
+            $IdUsuario: medida.IdUsuario,
+            $Valor:medida.Valor,
+            $Tiempo:medida.Tiempo,
+            $Latitud:medida.Latitud,
+            $Longitud:medida.Longitud
         }
-        cb(err)
-      })
-  } // ()
-
-  async insertarMedida(medida) {
-    var textoSQL =
-      'insert into Medidas values( $IdMedida, $IdTipoMedida , $IdUsuario, $Valor , $Tiempo , $Latitud , $Longitud);'
-    var valoresParaSQL = {
-      $IdMedida: null,
-      $IdTipoMedida: medida.IdTipoMedida,
-      $IdUsuario: medida.IdUsuario,
-      $Valor:medida.Valor,
-      $Tiempo:medida.Tiempo,
-      $Latitud:medida.Latitud,
-      $Longitud:medida.Longitud
-    }
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.run(textoSQL, valoresParaSQL, function(err) {
-        (err ? rechazar(err) : resolver())
-      })
-    })
-  }
-
-  async getLaUltimaMedidaPorUsuario(userId) {
-    var textoSQL = "SELECT * FROM Medidas WHERE IdUsuario="+userId+" ORDER BY IdMedida DESC LIMIT 0, 1";
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.all(textoSQL,
-        (err, res) => {
-          (err ? rechazar(err) : resolver(res))
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.run(textoSQL, valoresParaSQL, function(err) {
+                (err ? rechazar(err) : resolver())
+            })
         })
-    })
-  }
+    }
 
-  /*async GetLaUltimaMedida(idUsuario) {
+    async getLaUltimaMedidaPorUsuario(userId) {
+        var textoSQL = "SELECT * FROM Medidas WHERE IdUsuario="+userId+" ORDER BY IdMedida DESC LIMIT 0, 1";
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.all(textoSQL,
+                                (err, res) => {
+                (err ? rechazar(err) : resolver(res))
+            })
+        })
+    }
+
+    //------------------------------------------------------------
+    //     getMedidasPorFecha()
+    //------------------------------------------------------------
+
+    async getMedidasPorFecha() {
+        var textoSQL = "SELECT * FROM Medidas WHERE IdUsuario="+userId+" ORDER BY IdMedida DESC LIMIT 0, 1";
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.all(textoSQL,
+                                (err, res) => {
+                (err ? rechazar(err) : resolver(res))
+            })
+        })
+    }//getLaMedidasPorFecha
+
+
+    /*async GetLaUltimaMedida(idUsuario) {
     var textoSQL = "SELECT * FROM Medidas WHERE IdUsuario="+$idUsuario+" ORDER BY IdMedida DESC LIMIT 0, 1";
     return new Promise((resolver, rechazar) => {
       this.laConexion.all(textoSQL,
@@ -61,81 +76,81 @@ module.exports = class Logica {
     })
   }*/
 
-  async GetIdDelUsuario(email) {
-    var textoSQL = "SELECT * FROM Usuarios WHERE Email='"+email+"';";
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.all(textoSQL,
-        (err, res) => {
-          (err ? rechazar(err) : resolver(res))
+    async GetIdDelUsuario(email) {
+        var textoSQL = "SELECT * FROM Usuarios WHERE Email='"+email+"';";
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.all(textoSQL,
+                                (err, res) => {
+                (err ? rechazar(err) : resolver(res))
+            })
         })
-    })
-  }
+    }
 
-  async GetHashPorEmail(email) {
-//  var textoSQL = "select Password from Usuarios where Email = $email ";
-    var valoresParaSQL = {
-      $email: email
-    };
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.all(textoSQL,valoresParaSQL,
-        (err, res) => {
-          (err ? rechazar(err) : resolver(res))
+    async GetHashPorEmail(email) {
+        //  var textoSQL = "select Password from Usuarios where Email = $email ";
+        var valoresParaSQL = {
+            $email: email
+        };
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.all(textoSQL,valoresParaSQL,
+                                (err, res) => {
+                (err ? rechazar(err) : resolver(res))
+            })
         })
-    })
-  }
+    }
 
-async GetUsuarioPorEmail(email) {
-    var textoSQL = "select * from Usuarios where Email = $email ";
-    var valoresParaSQL = {  $email: email };
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.all(textoSQL,valoresParaSQL,
-          (err, res) => {
-          (err ? rechazar(err) : resolver(res))
+    async GetUsuarioPorEmail(email) {
+        var textoSQL = "select * from Usuarios where Email = $email ";
+        var valoresParaSQL = {  $email: email };
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.all(textoSQL,valoresParaSQL,
+                                (err, res) => {
+                (err ? rechazar(err) : resolver(res))
+            })
         })
-    })
-  }
+    }
 
-  insertarUsuario(datos) {
-    var textoSQL = "insert into Usuarios values( $IdUsuario, $Email, $Password, $Nombre, $Telefono, $TipoUsuario)";
-    var valoresParaSQL = {
-      $IdUsuario: null,
-      $Email: datos.Email,
-      $Password: datos.Password,
-      $Nombre: datos.Nombre,
-      $Telefono: datos.Telefono,
-      $TipoUsuario: datos.TipoUsuario
-    };
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.run(textoSQL, valoresParaSQL, function(err, res) {
-        (err ? rechazar(err) : resolver(res))
-      })
-     })
-  }
+    insertarUsuario(datos) {
+        var textoSQL = "insert into Usuarios values( $IdUsuario, $Email, $Password, $Nombre, $Telefono, $TipoUsuario)";
+        var valoresParaSQL = {
+            $IdUsuario: null,
+            $Email: datos.Email,
+            $Password: datos.Password,
+            $Nombre: datos.Nombre,
+            $Telefono: datos.Telefono,
+            $TipoUsuario: datos.TipoUsuario
+        };
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.run(textoSQL, valoresParaSQL, function(err, res) {
+                (err ? rechazar(err) : resolver(res))
+            })
+        })
+    }
 
-  async borrarFilasDe(tabla) {
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.run(
-        "delete from " + tabla + ";",
-        (err) => (err ? rechazar(err) : resolver())
-      )
-    })
-  } // ()
-  // .................................................................
-  // borrarFilasDeTodasLasTablas() -->
-  // .................................................................
-  async borrarFilasDeTodasLasTablas() {
-    await this.borrarFilasDe("Usuarios")
-  } // ()
-  // .................................................................
-  // cerrar() -->
-  // .................................................................
-  cerrar() {
-    return new Promise((resolver, rechazar) => {
-      this.laConexion.close((err) => {
-        (err ? rechazar(err) : resolver())
-      })
-    })
-  } // ()
+    async borrarFilasDe(tabla) {
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.run(
+                "delete from " + tabla + ";",
+                (err) => (err ? rechazar(err) : resolver())
+            )
+        })
+    } // ()
+    // .................................................................
+    // borrarFilasDeTodasLasTablas() -->
+    // .................................................................
+    async borrarFilasDeTodasLasTablas() {
+        await this.borrarFilasDe("Usuarios")
+    } // ()
+    // .................................................................
+    // cerrar() -->
+    // .................................................................
+    cerrar() {
+        return new Promise((resolver, rechazar) => {
+            this.laConexion.close((err) => {
+                (err ? rechazar(err) : resolver())
+            })
+        })
+    } // ()
 
 } // class
 // .....................................................................
