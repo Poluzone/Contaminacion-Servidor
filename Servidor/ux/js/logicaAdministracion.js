3db33929c24d6dcced008f8bad823e10ed9b3152
IviRome <ivan.romero.ruiz@hotmail.com>
2019-11-19 12:14:03 +0100

    Creada logicaAdministracion.js
    Contendrá los metodos relacionados con con obtener todos los sensores, buscar sensores y coger informacion de un sensor para mostrarla en el Front End

---
+++ Servidor/ux/js/logicaAdministracion.js
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------
+//----------------------- logicaAdministracion.js ---------------------
+//
+// Creada por Iván Romero Ruiz apartir del codigo de Josep Carreres
+// Fuster del archivo PROXY.js
+//
+//---------------------------------------------------------------------
+
+const ip_puerto = "http://localhost:8080";
+
+class logicaAdministracion {
+    
+  constructor() {
+      
+    console.log("logicaAdministracion funcionando");
+      
+  }
+    
+}
