48926a7e21e279a424d7f2b0caa5ee789a9c91f8
Emilia Rosa van der Heide <emvader@epsg.upv.es>
2019-12-04 17:22:29 +0100

    mas cambios para plesk

--- mainServidorREST.js
+++ mainServidorREST.js
@@ -6,7 +6,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const bcrypt = require('bcrypt')
-const Logica = require("../logica/Logica.js")
+const Logica = require("../Servidor/Logica.js")
 // .....................................................................
 // .....................................................................
 function cargarLogica(fichero) {
@@ -26,7 +26,7 @@ function cargarLogica(fichero) {
 // main()
 // .....................................................................
 async function main() {
-    var laLogica = await cargarLogica("../bd/datos.db")
+    var laLogica = await cargarLogica("../Servidor/bd/datos.db")
     // creo el servidor
     var servidorExpress = express()
     // para poder acceder a la carga de la petición http
