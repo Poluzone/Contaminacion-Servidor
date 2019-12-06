4b6f99f90017ee6e369dd2c52e12a019cd8791a3
IviRome <ivan.romero.ruiz@hotmail.com>
2019-12-06 23:15:20 +0100

    Borrados usuarios

--- ux/js/PROXY.js
+++ ux/js/PROXY.js
@@ -307,7 +307,8 @@ class Proxy {
             });
     }
 
-    //-------------------------------
+    //-------------Ivan--------------
+    // N: id de un sensor -->
     // borrarSensorPorID()
     //-------------------------------
     async borrarSensorPorID(id, callback) {
@@ -332,8 +333,33 @@ class Proxy {
     }
     
     //-------------Ivan--------------
+    // N: id de un usuario -->
+    // borrarSensorPorID()
+    //-------------------------------
+    async borrarUsuarioPorID(id, callback) {
+
+        /*var idJson = {
+            id: id
+        };*/
+
+        fetch(url + "/borrarUsuarioPorID", {
+            method: 'POST', // or 'PUT'
+            body: id.toString(), // data can be `string` or {object}!
+            headers: {
+                'User-Agent': 'jordi',
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            console.log(res)
+        }).catch(e => {
+            console.log("error: " + e);
+            return e;
+        });
+    }
+    
+    //-------------Ivan--------------
     // N: id de un sensor -->
-    // getNumeroUsuariosTotalesPorTipo()
+    // desvincularUsuarioDeSensorPorIdUsuario()
     //-------------------------------
     async desvincularUsuarioDeSensorPorIdUsuario(id){
         
