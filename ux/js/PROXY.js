8e5f87b6bda76e480fbc0a54c136cd812f58c15f
Jocarfl <jcarreresjr@hotmail.com>
2020-01-13 00:10:52 +0100

    update errores

--- ux/js/PROXY.js
+++ ux/js/PROXY.js
@@ -1,5 +1,10 @@
-const url = "http://localhost:8080";
-//const url = "https://juconol.upv.edu.es";
+//
+// PROXY.js
+// Creado por Josep
+//
+
+//const url = "http://localhost:8080";
+const url = "https://juconol.upv.edu.es";
 
 class Proxy {
     constructor() {
@@ -14,7 +19,7 @@ class Proxy {
             posicion: posicion
         };
 
-        fetch(URL + "/insertarMedida", {
+        fetch(url + "/insertarMedida", {
             method: 'POST', // or 'PUT'
             body: JSON.stringify(data), // data can be `string` or {object}!
             headers: {
@@ -35,7 +40,7 @@ class Proxy {
             Telefono: tel
         };
 
-        fetch(URL + "/insertarUsuario", {
+        fetch(url + "/insertarUsuario", {
             method: 'POST', // or 'PUT'
             body: JSON.stringify(data), // data can be `string` or {object}!
             headers: {
@@ -54,7 +59,7 @@ class Proxy {
     async ComprobacionLogin(data) {
         console.log("Realizando ComprobacionLogin");
 
-        fetch(URL + "/ComprobarLogin", {
+        fetch(url + "/ComprobarLogin", {
             method: 'POST', // or 'PUT'
             body: JSON.stringify(data), // data can be `string` or {object}!
             headers: {
@@ -70,11 +75,18 @@ class Proxy {
         }).then(function (datos) {
             console.log("hola2");
             if (datos.status == true) {
-                setCookie("username", datos.Usuario[0].Email);
-                console.log("Iniciar sesion correcto y se han creado los cookies");
-                checkCookie();
+                if (datos.Usuario[0].TipoUsuario == "Admin") {
+                    setCookie("username", datos.Usuario[0].Email);
+                    console.log("Iniciar sesion correcto y se han creado los cookies");
+                    checkCookie();
+                } else {
+                    console.log("El usuario que intenta iniciar sesión no es Admin");
+                    elUsuarioNoEsAdmin();
+                }
+
             } else {
                 console.log("No existe o no has puesto bien los datos");
+                usuarioOContraseñaIncorrectos();
             }
         });
     }
@@ -85,15 +97,15 @@ class Proxy {
     async GETultimaMedidaPorUsuario(userId, callback) {
 
         fetch(url + "/GETultimaMedidaPorUsuario", {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(userId), // data can be `string` or {object}!
-            headers: {
-                'User-Agent': 'jordi',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            return response.json();
-        })
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(userId), // data can be `string` or {object}!
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                return response.json();
+            })
             .then(function (datos) {
                 if (datos != undefined) {
                     console.log("Tenemos la última medida");
@@ -110,16 +122,16 @@ class Proxy {
     async GETidUsuario(username, callback) {
 
         fetch(url + "/GETidUsuario", {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(username), // data
-            headers: {
-                'User-Agent': 'jordi',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            console.log(response);
-            return response.json();
-        })
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(username), // data
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                console.log(response);
+                return response.json();
+            })
             .then(function (datos) {
                 if (datos != undefined) {
                     console.log("------------PROXY------------");
@@ -139,11 +151,11 @@ class Proxy {
         console.log("Llamada a getSensoresYSusUsuarios");
 
         fetch(url + "/getSensoresYSusUsuarios", {
-            method: 'GET', // or 'PUT'
-        }).then(function (response) {
-            console.log("response ", response);
-            return response.json();
-        })
+                method: 'GET', // or 'PUT'
+            }).then(function (response) {
+                console.log("response ", response);
+                return response.json();
+            })
             .then(function (datos) {
                 if (datos != undefined) {
 
@@ -169,15 +181,15 @@ class Proxy {
         console.log("Llamada a getNumSensoresSegunEstado con " + parseInt(estado));
 
         fetch(url + "/getNumSensoresSegunEstado/" + estado, {
-            method: 'GET', // or 'PUT'
-            headers: {
-                'User-Agent': 'jordi',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            console.log("response ", response);
-            return response.json();
-        })
+                method: 'GET', // or 'PUT'
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                console.log("response ", response);
+                return response.json();
+            })
             .then(function (datos) {
 
                 callback(datos);
@@ -198,11 +210,11 @@ class Proxy {
         console.log("Llamada a getTodosLosUsuariosYSusSensores");
 
         fetch(url + "/getTodosLosUsuariosYSusSensores", {
-            method: 'GET', // or 'PUT'
-        }).then(function (response) {
-            console.log("response ", response);
-            return response.json();
-        })
+                method: 'GET', // or 'PUT'
+            }).then(function (response) {
+                console.log("response ", response);
+                return response.json();
+            })
             .then(function (datos) {
                 if (datos != undefined) {
 
@@ -221,18 +233,18 @@ class Proxy {
 
     //------------Ivan---------------
     // getNumeroUsuariosTotales()
-    // --> N: numero de TODOS los usuarios 
+    // --> N: numero de TODOS los usuarios
     //-------------------------------
     async getNumeroUsuariosTotales(callback) {
 
         console.log("Llamada a getNumeroUsuariosTotales");
 
         fetch(url + "/getNumeroUsuariosTotales", {
-            method: 'GET', // or 'PUT'
-        }).then(function (response) {
-            console.log("response ", response);
-            return response.json();
-        })
+                method: 'GET', // or 'PUT'
+            }).then(function (response) {
+                console.log("response ", response);
+                return response.json();
+            })
             .then(function (datos) {
                 callback(datos);
 
@@ -253,15 +265,15 @@ class Proxy {
         console.log("Llamada a getNumeroUsuariosTotalesPorTipo con " + estado);
 
         fetch(url + "/getNumeroUsuariosTotalesPorTipo/" + estado, {
-            method: 'GET', // or 'PUT'
-            headers: {
-                'User-Agent': 'jordi',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            console.log("response ", response);
-            return response.json();
-        })
+                method: 'GET', // or 'PUT'
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                console.log("response ", response);
+                return response.json();
+            })
             .then(function (datos) {
 
                 callback(datos);
@@ -273,21 +285,93 @@ class Proxy {
 
     }
 
+    //-------------Ivan--------------
+    // N: id de un usuario -->
+    // borrarUsuarioPorID()
+    //-------------------------------
+    async borrarUsuarioPorID(id, callback) {
+
+        /*var idJson = {
+            id: id
+        };*/
+
+        fetch(url + "/borrarUsuarioPorID", {
+            method: 'POST', // or 'PUT'
+            body: id.toString(), // data can be `string` or {object}!
+            headers: {
+                'User-Agent': 'jordi',
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            console.log(res)
+        }).catch(e => {
+            console.log("error: " + e);
+            return e;
+        });
+    }
+
+    //-------------Ivan--------------
+    // N: id de un usuario -->
+    // borrarUsuarioPorID()
+    //-------------------------------
+    async borrarUsuarioPorID(id, callback) {
+
+        /*var idJson = {
+            id: id
+        };*/
+
+        fetch(url + "/borrarUsuarioPorID", {
+            method: 'POST', // or 'PUT'
+            body: id.toString(), // data can be `string` or {object}!
+            headers: {
+                'User-Agent': 'jordi',
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            console.log(res)
+        }).catch(e => {
+            console.log("error: " + e);
+            return e;
+        });
+    }
+
+    //-------------Ivan--------------
+    // N: id de un sensor -->
+    // desvincularUsuarioDeSensorPorIdUsuario()
+    //-------------------------------
+    async desvincularUsuarioDeSensorPorIdUsuario(id) {
+
+        fetch(url + "/desvincularUsuarioDeSensorPorIdUsuario", {
+            method: 'POST', // or 'PUT'
+            body: id.toString(), // data can be `string` or {object}!
+            headers: {
+                'User-Agent': 'jordi',
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            console.log(res)
+        }).catch(e => {
+            console.log("error: " + e);
+            return e;
+        });
+
+    }
+
     //-------------------------------
     // GetUltimasMedidasPorFecha()
     //-------------------------------
     async getTodasLasMedidasPorFecha(intervalo, callback) {
 
         fetch(url + "/GetTodasLasMedidasPorFecha", {
-            method: 'POST', // or 'PUT'
-            body: JSON.stringify(intervalo), // data can be `string` or {object}!
-            headers: {
-                'User-Agent': 'jordi',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            return response.json();
-        })
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(intervalo), // data can be `string` or {object}!
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                return response.json();
+            })
             .then(function (datos) {
                 if (datos != undefined) {
                     console.log("Tenemos las última medidas desde: " + intervalo.desde + " hasta " + intervalo.hasta);
@@ -353,22 +437,22 @@ class Proxy {
 
     //-------------------------------
     // Emilia Rosa van der Heide
-    // -> getEstacionesOficiales() -> JSON  
-    // con las estaciones y las medidas  
+    // -> getEstacionesOficiales() -> JSON
+    // con las estaciones y las medidas
     // de Gandia
     //-------------------------------
     async getEstacionesOficiales(callback) {
         console.log("PROXY: getEstacionesOficiales")
 
         fetch(url + "/getMedidasEstacionOficialGandia", {
-            method: 'POST', // or 'PUT'
-            headers: {
-                'User-Agent': 'jordi',
-                'Content-Type': 'application/json'
-            }
-        }).then(function (response) {
-            return response.json();
-        })
+                method: 'POST', // or 'PUT'
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                return response.json();
+            })
             .then(function (datos) {
                 if (datos != undefined) {
                     console.log(datos)
@@ -380,4 +464,55 @@ class Proxy {
             });
     }
 
+    /****************************************************************************************
+    -->
+    getTodosErroresDeSensoresSinRevision()
+    --> lista<sensor, usuario, error>
+
+    Devuelve una lista con los sensores que han medido erroneamente durante mas de 4 horas
+    y su información así como la del usuario que lo posee
+
+    Ivan
+    ****************************************************************************************/
+    async getTodosErroresDeSensoresSinRevision(callback) {
+        console.log("PROXY: getTodosErroresDeSensoresSinRevision")
+
+        fetch(url + "/getTodosErroresDeSensoresSinRevision", {
+                method: 'GET', // or 'PUT'
+                headers: {
+                    'User-Agent': 'jordi',
+                    'Content-Type': 'application/json'
+                }
+            }).then(function (response) {
+                return response.json();
+            })
+            .then(function (datos) {
+                //console.log("holis " + JSON.stringify(datos));
+                callback(datos);
+            });
+    }
+
+    /****************************************************************************************
+    -->
+    marcarErroresComoRevisados()
+    -->
+
+    Cambia el campo Revisado de ErrorSensor de la base de datos para que no aparezca otra vez
+
+    Ivan
+    ****************************************************************************************/
+    async marcarErroresComoRevisados(){
+        fetch(url + "/marcarErroresComoRevisados", {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'User-Agent': 'jordi',
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            console.log(res)
+        }).catch(e => {
+            console.log("error: " + e);
+            return e;
+        });
+    }
 }
