598c3e7dc011d1be4c11e9de8dcc93b2b8238d9c
Jocarfl <jcarreresjr@hotmail.com>
2020-01-12 17:26:39 +0100

    Arreglar marcar todos como reviados

--- mainServidorREST.js
+++ mainServidorREST.js
@@ -11,55 +11,66 @@ const puerto = process.env.PORT || 8080;
 // .....................................................................
 // .....................................................................
 function cargarLogica(fichero) {
-    return new Promise((resolver, rechazar) => {
-        var laLogica = new Logica(fichero,
-            function (err) {
-                if (err) {
-                    rechazar(err)
-                } else {
-                    resolver(laLogica)
-                }
+  return new Promise((resolver, rechazar) => {
+    var laLogica = new Logica(fichero,
+      function(err) {
+        if (err) {
+          rechazar(err)
+        } else {
+          resolver(laLogica)
+        }
 
-            }) // new
-    }) // Promise
+      }) // new
+  }) // Promise
 } // ()
 // .....................................................................
 // main()
 // .....................................................................
 async function main() {
-    var laLogica = await cargarLogica("./bd/datos.db")
-    // creo el servidor
-    var servidorExpress = express()
-    // para poder acceder a la carga de la petición http
-    // asumiendo que es JSON
-    servidorExpress.use(bodyParser.text({
-        type: 'application/json'
-    }))
+  var laLogica = await cargarLogica("./bd/datos.db")
+  // creo el servidor
+  var servidorExpress = express()
+  // para poder acceder a la carga de la petición http
+  // asumiendo que es JSON
+  servidorExpress.use(bodyParser.text({
+    type: 'application/json'
+  }))
 
-    //Pa que funcione getSensoresYSusUsuarios desde la web
-    servidorExpress.use((req, res, next) => {
-        // Permitir cross origin
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //Pa que funcione getSensoresYSusUsuarios desde la web
+  servidorExpress.use((req, res, next) => {
+    // Permitir cross origin
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
-        // Monitorear todas la peticiones
-        //console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body)
-        next()
-    })
+    // Monitorear todas la peticiones
+    //console.log(`${new Date().toString()} => ${req.originalUrl}`, req.body)
+    next()
+  })
 
 
-    // cargo las reglas REST
-    var reglas = require("./ReglasREST.js")
-    reglas.cargar(servidorExpress, laLogica, bcrypt)
-    // arranco el servidor
-    var servicio = servidorExpress.listen(puerto, function () {
-        console.log("servidor REST escuchando en el puerto 8080 ")
-    })
-    // capturo control-c para cerrar el servicio ordenadamente
-    process.on('SIGINT', function () {
-        console.log(" terminando ")
-        servicio.close()
-    })
+  // cargo las reglas REST
+  var reglas = require("./ReglasREST.js")
+  reglas.cargar(servidorExpress, laLogica, bcrypt)
+  // arranco el servidor
+  var servicio = servidorExpress.listen(puerto, function() {
+    console.log("servidor REST escuchando en el puerto 8080 ")
+  })
+/*
+  var now = new Date();
+  var milis = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 00, 0, 0, 0) - now;
+  if (milis < 0) {
+    milis += 86400000; // it's after 10am, try 10am tomorrow.
+  }
+  setTimeout(function() {
+    laLogica.comprobarSiHayErroresDeMedicionEnSensor();
+  }, milis);
+
+  */
+  // capturo control-c para cerrar el servicio ordenadamente
+  process.on('SIGINT', function() {
+    console.log(" terminando ")
+    servicio.close()
+  })
 } // ()
 // .....................................................................
 // .....................................................................
